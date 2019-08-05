816103711fbe2069b22fba9115d7ed233677ebc1
Hannes Dahlberg <hannes@dwik.se>
2019-08-05 14:30:17 +0200

    End point for creating project

--- src/api/controllers/project.controller.ts
+++ src/api/controllers/project.controller.ts
@@ -2,12 +2,31 @@
 import { RequestHandler } from "express";
 import { ServiceModule } from "simplyserveme";
 
-import { controller } from "./controller";
+import { controller, Controller } from "./controller";
+import { ICreateProjectJSON } from "../../shared/dto/create-project.dto";
+import { CustomerEntity } from "../entities/customer.entity";
+import { reject } from "bluebird";
+import { ProjectEntity } from "../entities/project.entity";
 
 export class ProjectController extends ServiceModule {
   public index(): RequestHandler {
     return controller((handler) => {
       handler.response<{ foo: string }>().json({ foo: "bar" });
+    });
+  }
+  public create(): RequestHandler {
+    return controller((handler: Controller) => {
+      const body = handler.body<ICreateProjectJSON>();
+      CustomerEntity.findOne(body.customerId).then((customer?: CustomerEntity) => {
+        if (customer !== undefined) {
+          ProjectEntity.create({ name: body.name, customer })
+            .save()
+            .then(() => handler.sendStatus(200))
+            .catch((error: any) => handler.sendStatus(500));
+        } else {
+          handler.sendStatus(500);
+        }
+      }).catch((error: any) => handler.sendStatus(500));
     });
   }
 }
--- src/shared/dto/create-project.dto.ts
+++ src/shared/dto/create-project.dto.ts
@@ -1,31 +1,27 @@
 import DTO from "./dto";
 
-export interface ICreateProject<A, B> {
-  id: number;
-  foo?: A;
-  bar?: B[];
+export interface ICreateProject {
+  name: string;
+  customerId: number;
 }
 
-export interface ICreateProjectDTO extends ICreateProject<ICreateProjectDTO, ICreateProjectDTO> { }
-export interface ICreateProjectJSON extends ICreateProject<ICreateProjectJSON, ICreateProjectJSON> { }
+export interface ICreateProjectDTO extends ICreateProject { }
+export interface ICreateProjectJSON extends ICreateProject { }
 
 export class CreateProjectDTO extends DTO<ICreateProjectDTO> implements ICreateProjectDTO {
   public static parse(object: ICreateProjectJSON): CreateProjectDTO {
     return new CreateProjectDTO({
-      id: object.id,
-      ...(object.foo ? { foo: CreateProjectDTO.parse(object.foo) } : null),
-      ...(object.bar ? { projects: object.bar.map((bar: ICreateProjectJSON) => CreateProjectDTO.parse(bar)) } : null),
+      name: object.name,
+      customerId: object.customerId,
     });
   }
-  public id!: number;
-  public foo?: CreateProjectDTO;
-  public bar?: CreateProjectDTO[];
+  public name!: string;
+  public customerId!: number;
 
   public serialize(): ICreateProjectJSON {
     return {
-      id: this.id,
-      ...(this.foo ? { foo: this.foo.serialize() } : null),
-      ...(this.bar ? { bar: this.bar.map((foo: CreateProjectDTO) => foo.serialize()) } : null),
+      name: this.name,
+      customerId: this.customerId,
     };
   }
 }
