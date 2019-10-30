eba6448e241eca2f055441bf2c421624416bf7f0
Hannes Dahlberg <hannes@harvey-pro.local>
2019-10-30 09:08:08 +0100

    Typeorm config updates

--- src/typeorm.ts
+++ src/typeorm.ts
@@ -5,14 +5,15 @@ import { ConnectionOptions, createConnection } from "typeorm";
 import { BannedTokenEntity } from "./api/entities/banned-token.entity";
 import { CustomerEntity } from "./api/entities/customer.entity";
 import { GroupEntity } from "./api/entities/group.entity";
+import { ProjectUserEntity } from "./api/entities/project-user.entity";
 import { ProjectEntity } from "./api/entities/project.entity";
 import { ResourceEntity } from "./api/entities/resource.entity";
 import { TagEntity } from "./api/entities/tag.entity";
+import { TaskUserEntity } from "./api/entities/task-user.entity";
 import { TaskEntity } from "./api/entities/task.entity";
 import { TextEntity } from "./api/entities/text.entity";
 import { TimeEntity } from "./api/entities/time.entity";
 import { TokenEntity } from "./api/entities/token.entity";
-import { UserProjectEntity } from "./api/entities/user-project.entity";
 import { UserEntity } from "./api/entities/user.entity";
 
 export const defaultConnectionConfig: ConnectionOptions = {
@@ -24,15 +25,16 @@ export const defaultConnectionConfig: ConnectionOptions = {
     BannedTokenEntity,
     CustomerEntity,
     GroupEntity,
+    ProjectUserEntity,
     ProjectEntity,
     ResourceEntity,
-    TextEntity,
-    TaskEntity,
     TagEntity,
+    TaskUserEntity,
+    TaskEntity,
+    TextEntity,
     TimeEntity,
     TokenEntity,
     UserEntity,
-    UserProjectEntity,
   ],
   migrations: [
     "./migrations/*.js",
